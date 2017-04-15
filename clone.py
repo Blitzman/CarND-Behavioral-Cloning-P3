3b9f992e70d324ad301838b9109fd275141a393c
agarcia@dtic.ua.es <Albert Garca>
2017-04-15 13:38:17 +0200

    add loss visualization

--- clone.py
+++ clone.py
@@ -8,6 +8,9 @@ from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Cropping2D
 
+from keras.models import Model
+import matplotlib.pyplot as plt
+
 lines = []
 with open('recorded_data/fast_1lap/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
@@ -39,19 +42,31 @@ X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
 model = Sequential()
-model.add(Cropping2D(cropping=((50, 20), (0,0)), input_shape=(160, 320, 3)))
-model.add(Lambda(lambda x: (x / 255.0) - 0.5))
-model.add(Convolution2D(6, 5, 5, activation="relu"))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6, 5, 5, activation="relu"))
-model.add(MaxPooling2D())
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70, 25), (0,0))))
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=8)
+history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+
+print(history_object.history.keys())
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
 
 model.save('model.h5')
+
 exit()
