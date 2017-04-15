c941e85fbc7b709345643aa6da2ffa3e35f2713e
agarcia@dtic.ua.es <Albert Garca>
2017-04-15 13:17:48 +0200

    add basic augmentations with flipping

--- clone.py
+++ clone.py
@@ -6,6 +6,7 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
 from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
+from keras.layers import Cropping2D
 
 lines = []
 with open('recorded_data/fast_1lap/driving_log.csv') as csvfile:
@@ -26,11 +27,20 @@ for line in lines:
     measurement = float(line[3])
     measurements.append(measurement)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+augmented_images = []
+augmented_measurements = []
+for image, measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image, 1))
+    augmented_measurements.append(measurement * -1.0)
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
 model = Sequential()
-model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((50, 20), (0,0)), input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5))
 model.add(Convolution2D(6, 5, 5, activation="relu"))
 model.add(MaxPooling2D())
 model.add(Convolution2D(6, 5, 5, activation="relu"))
