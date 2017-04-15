d0b9447f108d7fc812a3f271bb6840e874a09995
agarcia@dtic.ua.es <Albert Garca>
2017-04-15 15:04:32 +0200

    modify network to be LeNet-5

--- clone.py
+++ clone.py
@@ -4,6 +4,8 @@ import numpy as np
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
 
 lines = []
 with open('recorded_data/fast_1lap/driving_log.csv') as csvfile:
@@ -28,12 +30,18 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 model = Sequential()
-model.add(Lambda(lambda x: x / 255.0, input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
+model.add(Convolution2D(6, 5, 5, activation="relu"))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6, 5, 5, activation="relu"))
+model.add(MaxPooling2D())
 model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=32)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=8)
 
 model.save('model.h5')
 exit()
