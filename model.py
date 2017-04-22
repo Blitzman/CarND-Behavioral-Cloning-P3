3942cbf1e4b5025f08ea5b6dc6b5730331babac9
agarcia@dtic.ua.es <Albert Garca>
2017-04-22 19:53:34 +0200

    add generator, ELU activations, refactor model for generator support

--- model.py
+++ model.py
@@ -2,6 +2,8 @@ import csv
 import cv2
 import sklearn
 import numpy as np
+import random
+import math
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
@@ -11,33 +13,39 @@ from keras.layers import MaxPooling2D
 from keras.layers import Cropping2D
 from keras.layers import Dropout
 from keras.layers.advanced_activations import ELU
-from keras.regularizers import l2, activity_l2
+from keras.regularizers import l2
 from keras.callbacks import EarlyStopping
 from keras.optimizers import Adam
+from keras.utils.visualize_util import plot
 
 from keras.models import Model
 import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
 
 def preprocess_image(img):
 
     preprocessed_img = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
     return preprocessed_img
 
-def load_sample(sample, img_path):
+def augment_sample(img, steering):
+
+    augmented_image = cv2.flip(img, 1)
+    augmented_steering = steering * -1.0
+    return augmented_image, augmented_steering
+
+def load_sample(sample):
 
     images = []
     steerings = []
 
-    source_path_center = sample[0]
-    filename_center = source_path_center.split('/')[-1]
-    source_path_left = sample[1]
-    filename_left = source_path_left.split('/')[-1]
-    source_path_right = sample[2]
-    filename_right = source_path_right.split('/')[-1]
+    filename_center = sample[0]
+    filename_left = sample[1]
+    filename_right = sample[2]
 
-    image_center = cv2.imread(img_path + filename_center)
-    image_left = cv2.imread(img_path + filename_left)
-    image_right = cv2.imread(img_path + filename_right)
+    image_center = cv2.imread(filename_center)
+    image_left = cv2.imread(filename_left)
+    image_right = cv2.imread(filename_right)
 
     image_center = preprocess_image(image_center)
     image_left = preprocess_image(image_left)
@@ -58,13 +66,13 @@ def load_sample(sample, img_path):
 
     return images, steerings
 
-def generator(samples, batch_size=64):
+def generator(samples, batch_size=64, augment=False):
 
     num_samples = len(samples)
 
     while 1:
 
-        shuffle(samples)
+        sklearn.utils.shuffle(samples)
         for offset in range(0, num_samples, batch_size):
 
             batch_samples = samples[offset:offset + batch_size]
@@ -74,26 +82,67 @@ def generator(samples, batch_size=64):
 
             for batch_sample in batch_samples:
 
-                sample_images, sample_steerings = load_sample(batch_sample[0], batch_sample[1])
+                sample_images, sample_steerings = load_sample(batch_sample)
                 batch_images.extend(sample_images)
                 batch_steerings.extend(sample_steerings)
+
+            if augment:
+
+                augmented_images = []
+                augmented_steerings = []
+
+                for batch_image, batch_steering in zip(batch_images, batch_steerings):
+
+                    if random.uniform(0, 1) > 0.66:
+                        augmented_image, augmented_steering = augment_sample(batch_image, batch_steering)
+                        augmented_images.append(augmented_image)
+                        augmented_steerings.append(augmented_steering)
+                    else:
+                        augmented_images.append(batch_image)
+                        augmented_steerings.append(batch_steering)
+
+                batch_images = augmented_images
+                batch_steerings = augmented_steerings
 
             X = np.array(batch_images)
             y = np.array(batch_steerings)
             yield sklearn.utils.shuffle(X, y)
 
 ## Specify list of CSV files to read with their corresponding image paths
-csv_paths = ['data/',
-        'recorded_data/recovery1/',
-        'recorded_data/c_recovery1/',
-        'recorded_data/t2_rightlane1/']
-img_paths = ['data/IMG/',
-        'recorded_data/recovery1/IMG/',
-        'recorded_data/c_recovery1/IMG/',
-        'recorded_data/t2_rightlane1/IMG/']
 
-## Generate list of samples as [line, image path]
+csv_paths = ['data/',
+        'recorded_data/t1_center1/',
+        'recorded_data/t1_recovery1/',
+        'recorded_data/t1_c_recovery1/',
+        'recorded_data/t2_smoothturn1/',
+        'recorded_data/t2_center1/',
+        'recorded_data/t2_centerslow1/',
+        'recorded_data/t2_center2/',
+        'recorded_data/t2_center3/',
+        'recorded_data/t2_c_center1/',
+        'recorded_data/t2_recovery1/',
+        'recorded_data/t2_recoveryend1/',
+        'recorded_data/t2_recoveryturn1/',
+        'recorded_data/t2_c_recovery1/']
+img_paths = ['data/IMG/',
+        'recorded_data/t1_center1/IMG/',
+        'recorded_data/t1_recovery1/IMG/',
+        'recorded_data/t1_c_recovery1/IMG/',
+        'recorded_data/t2_smoothturn1/IMG/',
+        'recorded_data/t2_center1/IMG/',
+        'recorded_data/t2_centerslow1/IMG/',
+        'recorded_data/t2_center2/IMG/',
+        'recorded_data/t2_center3/IMG/',
+        'recorded_data/t2_c_center1/IMG/',
+        'recorded_data/t2_recovery1/IMG/',
+        'recorded_data/t2_recoveryend1/IMG/',
+        'recorded_data/t2_recoveryturn1/IMG/',
+        'recorded_data/t2_c_recovery1/IMG/']
+
+## Generate list of samples including image paths
+
 samples = []
+steerings = []
 for csv_path, img_path in zip(csv_paths, img_paths):
 
     print('Sampling ' + csv_path)
@@ -106,76 +155,103 @@ for csv_path, img_path in zip(csv_paths, img_paths):
             lines.append(line)
         lines = lines[1:]
 
-    for line in lines:
-        samples.append([line, img_path])
-
-images = []
-steerings = []
-
-for csv_path, img_path in zip(csv_paths, img_paths):
-
-    print('Processing CSV: ' + csv_path)
-    lines = []
-
-    with open(csv_path + 'driving_log.csv') as csvfile:
-
-        reader = csv.reader(csvfile)
-        for line in reader:
-            lines.append(line)
-        lines = lines[1:]
+    print(str(len(lines)) + ' samples...')
 
     for line in lines:
+        sample = line
+        sample[0] = img_path + sample[0].split('/')[-1]
+        sample[1] = img_path + sample[1].split('/')[-1]
+        sample[2] = img_path + sample[2].split('/')[-1]
+        samples.append(sample)
+        steerings.append(float(sample[3]))
 
-        sample_images, sample_steerings = load_sample(line, img_path)
-        images.extend(sample_images)
-        steerings.extend(sample_steerings)
+## Generate training and testing sets with generators
+steerings = np.array(steerings)
+num_bins = 8
+avg_samples_per_bin = len(steerings)/num_bins
+hist, bins = np.histogram(steerings, num_bins)
+width = 0.7 * (bins[1] - bins[0])
+center = (bins[:-1] + bins[1:]) / 2
+plt.bar(center, hist, align='center', width=width)
+plt.plot((np.min(steerings), np.max(steerings)), (avg_samples_per_bin, avg_samples_per_bin), 'k-')
+plt.show()
 
+keep_probs = []
+target = avg_samples_per_bin * .5
+for i in range(num_bins):
+    if hist[i] < target:
+        keep_probs.append(1.)
+    else:
+        keep_probs.append(1./(hist[i]/target))
 
-## Data augmentation
+remove_list = []
+for i in range(len(steerings)):
+    for j in range(num_bins):
+        if steerings[i] > bins[j] and steerings[i] <= bins[j+1]:
+            # delete from X and y with probability 1 - keep_probs[j]
+            if np.random.rand() > keep_probs[j]:
+                remove_list.append(i)
 
-augmented_images = []
-augmented_steerings = []
+#samples = np.delete(samples, remove_list, axis=0)
+steerings = np.delete(steerings, remove_list, axis=0)
 
-for image, steering in zip(images, steerings):
-    augmented_images.append(image)
-    augmented_steerings.append(steering)
-    augmented_images.append(cv2.flip(image, 1))
-    augmented_steerings.append(steering * -1.0)
+hist, bins = np.histogram(steerings, num_bins)
+plt.bar(center, hist, align='center', width=width)
+plt.plot((np.min(steerings), np.max(steerings)), (avg_samples_per_bin, avg_samples_per_bin), 'k-')
+plt.show()
 
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_steerings)
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+print('Training with ' + str(len(train_samples)) + ' samples...')
+print('Validating with ' + str(len(validation_samples)) + ' samples...')
+
+train_generator = generator(train_samples, batch_size=64, augment=True)
+validation_generator = generator(validation_samples, batch_size=64, augment=False)
 
 ## Model definition
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 127.5) - 1.0, input_shape=(160, 320, 3)))
-model.add(Cropping2D(cropping=((70, 25), (0,0))))
-model.add(Convolution2D(24, 5, 5, subsample=(2, 2)))
-model.add(Activation('relu'))
-model.add(Convolution2D(36, 5, 5, subsample=(2, 2)))
-model.add(Activation('relu'))
-model.add(Convolution2D(48, 5, 5, subsample=(2, 2)))
-model.add(Activation('relu'))
-model.add(Convolution2D(64, 3, 3))
-model.add(Activation('relu'))
-model.add(Convolution2D(64, 3, 3))
-model.add(Activation('relu'))
+model.add(Cropping2D(cropping=((50, 20), (0,0))))
+model.add(Convolution2D(24, 5, 5, W_regularizer=l2(0.001), subsample=(2, 2)))
+#model.add(Activation('relu'))
+model.add(ELU())
+model.add(Convolution2D(36, 5, 5, W_regularizer=l2(0.001), subsample=(2, 2)))
+#model.add(Activation('relu'))
+model.add(ELU())
+model.add(Convolution2D(48, 5, 5, W_regularizer=l2(0.001), subsample=(2, 2)))
+#model.add(Activation('relu'))
+model.add(ELU())
+model.add(Convolution2D(64, 3, 3, W_regularizer=l2(0.001)))
+#model.add(Activation('relu'))
+model.add(ELU())
+model.add(Convolution2D(64, 3, 3, W_regularizer=l2(0.001)))
+#model.add(Activation('relu'))
+model.add(ELU())
 model.add(Flatten())
-model.add(Dense(100))
-model.add(Dropout(0.5))
-model.add(Activation('relu'))
-model.add(Dense(50))
-model.add(Dropout(0.5))
-model.add(Activation('relu'))
-model.add(Dense(10))
-model.add(Dropout(0.5))
-model.add(Activation('relu'))
+model.add(Dense(100, W_regularizer=l2(0.001)))
+#model.add(Dropout(0.5))
+#model.add(Activation('relu'))
+model.add(ELU())
+model.add(Dense(50, W_regularizer=l2(0.001)))
+#model.add(Dropout(0.5))
+#model.add(Activation('relu'))
+model.add(ELU())
+model.add(Dense(10, W_regularizer=l2(0.001)))
+#model.add(Dropout(0.5))
+#model.add(Activation('relu'))
+model.add(ELU())
 model.add(Dense(1))
 
-model.compile(loss='mse', optimizer=Adam(lr=1e-4))
+model.compile(loss='mse', optimizer=Adam(lr=1e-3))
+
+model.summary()
 
 early_stopping = EarlyStopping(monitor='val_loss', patience=0, verbose=0, mode='auto')
-history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=32, callbacks=[early_stopping])
+#history_object = model.fit(X_train, y_train, validation_split=0.05, shuffle=True, nb_epoch=32, callbacks=[early_stopping])
+
+history_object = model.fit_generator(train_generator, samples_per_epoch=len(train_samples),
+        validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=128, callbacks=[early_stopping])
 
 print(history_object.history.keys())
 plt.plot(history_object.history['loss'])
